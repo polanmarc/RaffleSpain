c7e67ad79d0f455d8522385d05cc43a76b7c9e8a
polanmarc <polanmarc19@gmail.com>
2023-11-27 21:32:11 +0100

    Modificacion header grid

--- public/js/script.js
+++ public/js/script.js
@@ -1,24 +1,3 @@
-// // Animacion cuando cliques un ancor que vaya bajando poco a poco
-
-// document.querySelectorAll('header a').forEach(anchor => {
-//     if (document.querySelector("#main_page")) {
-//         anchor.addEventListener('click', function (e) {
-//             e.preventDefault();
-//             const targetId = this.getAttribute('href').substring(1);
-//             const targetElement = document.getElementById(targetId);
-    
-//             if (targetElement) {
-//                 targetElement.scrollIntoView({
-//                     behavior: 'smooth',
-//                     block: 'start',
-//                     inline: 'center'
-//                 });
-//             }
-//         });
-//     }
-// });
-
-
 // // Animacion entrar pagina que salga el elemento y modificaciones grid 
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -73,7 +52,7 @@ const media = window.matchMedia("(min-width: 1024px)");
 function mirarTamaño(media) {
     if (media.matches) {
         manuAparecer.style.display = 'none';
-        menu.style.display = 'block';
+        menu.style.display = 'flex';
     }
     else {
         menu.style.display = 'none';
@@ -88,9 +67,9 @@ media.addEventListener("change", mirarTamaño)
 img.addEventListener('click', () => {
     let styleMenu = window.getComputedStyle(manuAparecer);
     if (styleMenu.getPropertyValue("display") === 'none') {
-        manuAparecer.style.display = 'block';
+        manuAparecer.style.display = 'flex';
         menu.style.display = 'none';
-    } else if (styleMenu.getPropertyValue("display") === 'block') {
+    } else if (styleMenu.getPropertyValue("display") === 'flex') {
         manuAparecer.style.display = 'none';
     }
 });
